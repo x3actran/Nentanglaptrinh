0df4a55f8e257eb604e4a29b0df4961fa9ca7492
Angel <81817917+x3actran@users.noreply.github.com>
2021-08-24 19:19:50 +0700

    angel

--- buổi 19/tudien.js
+++ buổi 19/tudien.js
@@ -1,13 +1,13 @@
 function transcript() {
     let arr_eng = ["banana", "apple", "orange", "pen", "pencil", "book"];
-    let arr_vie = ["chuối", "táo", "cam", "bút", "bút chì", "sách"];
+    let arr_viet = ["chuối", "táo", "cam", "bút", "bút chì", "sách"];
     let string = document.getElementById("string").value;
     let index_eng = 0;
     let result = "";
     for (let i = 0; i < arr_eng.length; i++) {
         if (string == arr_eng[i]) {
             index_eng = i;
-            for(let j = 0; j < arr_vie.length; j++) {
+            for(let j = 0; j < arr_viet.length; j++) {
                 if (index_eng == j) {
                     result = arr_vie[j];
                 }
